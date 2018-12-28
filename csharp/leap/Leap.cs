b85ad8f6a824d341a6bcf0b85a555f2e0bb6821e
Common Lisper <atosdo@live.com>
2018-12-28 23:59:05 +0200

    Refactor Leap exercise

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -1,14 +1,6 @@
-using System;
-
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-        {
-            return true;
-        }
+    public static bool IsLeapYear(int year) =>
+        (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
 
-        return false;
-    }
 }
